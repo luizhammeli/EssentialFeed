ff4de2f72517a11f0152c281e8c970191b127e14
Luiz Hammerli <luizhammerli@gmail.com>
2021-11-02 23:43:42 -0300

    - Create initial project structure

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Luiz Diniz Hammerli on 29/10/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let imageURL: URL
+    let description: String?
+    let location: String?
+}
