0da25424b7d639f2018592912d25d6dd1d206c96
Luiz Hammerli <luizhammerli@gmail.com>
2021-11-21 08:30:01 -0300

    - Save command cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -14,17 +14,41 @@ final class LocalFeedLoader {
     init(store: FeedStore) {
         self.store = store
     }
+    
+    func save(items: [FeedItem]) {
+        store.deleteCachedItems()
+    }
 }
 
 final class FeedStore {
-    var deleteCacheFeedCallCount = 0
+    private(set) var deleteCacheFeedCallCount = 0
+    
+    func deleteCachedItems() {
+        deleteCacheFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
-    func test_() {
+    func test_init_doesNotDeleteCacheUponCreation() {
         let feedStore = FeedStore()
         let _ = LocalFeedLoader(store: feedStore)
         
         XCTAssertEqual(feedStore.deleteCacheFeedCallCount, 0)
+    }
+    
+    func test_init_requestCacheDeletion() {
+        let feedStore = FeedStore()
+        let sut = LocalFeedLoader(store: feedStore)
+        sut.save(items: [anyUniqueFeedItem(), anyUniqueFeedItem()])
+        
+        XCTAssertEqual(feedStore.deleteCacheFeedCallCount, 1)
+    }
+}
+
+// MARK: - Helpers
+
+extension CacheFeedUseCaseTests {
+    private func anyUniqueFeedItem() -> FeedItem {
+        return .init(id: UUID(), imageURL: makeURL(), description: nil, location: nil)
     }
 }
