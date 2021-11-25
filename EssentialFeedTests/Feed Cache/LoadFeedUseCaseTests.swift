69521c28666bc2b282a59f149caef30de267dee1
Luiz Hammerli <luizhammerli@gmail.com>
2021-11-25 10:02:34 -0300

    - Add LoadFeedUseCaseTests

---
+++ EssentialFeedTests/Feed Cache/LoadFeedUseCaseTests.swift
@@ -0,0 +1,13 @@
+//
+//  LoadFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Luiz Diniz Hammerli on 25/11/21.
+//
+
+import XCTest
+import EssentialFeed
+
+final class LoadFeedUseCaseTests: XCTestCase {
+    
+}
