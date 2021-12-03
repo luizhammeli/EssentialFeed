eeb9592ba221cc962a64bf1e83743800b02d6ead
Luiz Hammerli <luizhammerli@gmail.com>
2021-12-02 23:20:51 -0300

    - Set as internal

--- EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -10,11 +10,11 @@ import Foundation
 final class FeedCachePolicy {
     private init() {}
     
-    static private var maxCacheAgeInDays: Int {
+    private static var maxCacheAgeInDays: Int {
         return 7
     }
     
-    static func isValid(_ timestamp: Date, against date: Date) -> Bool {
+    internal static func isValid(_ timestamp: Date, against date: Date) -> Bool {
         guard let limitDate = Calendar(identifier: .gregorian).date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
         return date < limitDate
     }
