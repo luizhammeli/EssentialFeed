0b7f6a32c7d8b402572f0d66dfe22ddd5f8246ed
Luiz Hammerli <luizhammerli@gmail.com>
2021-12-02 23:18:39 -0300

    - Set FeedCachePoilicy init method as private to avoid instance

--- EssentialFeed/Feed Cache/FeedCachePolicy.swift
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -8,12 +8,14 @@
 import Foundation
 
 final class FeedCachePolicy {
+    private init() {}
+    
     static private var maxCacheAgeInDays: Int {
         return 7
     }
     
-    static func isValid(_ timestamp: Date, against currentDate: Date) -> Bool {
+    static func isValid(_ timestamp: Date, against date: Date) -> Bool {
         guard let limitDate = Calendar(identifier: .gregorian).date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
-        return currentDate < limitDate
+        return date < limitDate
     }
 }
